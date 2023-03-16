ebf2e09a11e0b362dbf31ca0dce8ee4e83c49d47
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-03-16 09:07:44 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - alwayss the start of a c program
+*
+*Return always 0 (successful)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
