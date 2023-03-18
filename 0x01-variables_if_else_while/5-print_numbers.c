90b5ac2c31778355573471d815bebf9c769a30f3
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-03-18 04:41:05 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - stat of the program
+  *Description  prints all numbers below 10
+  *Return: returns 0 as success
+ */
+
+/*program starts here*/
+int main(void)
+{
+	int num;
+
+	/*for loop to prints numbers below 10*/
+	for (num = 0; num < 10; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
