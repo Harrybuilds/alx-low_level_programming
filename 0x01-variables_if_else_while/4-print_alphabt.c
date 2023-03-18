74a547232a2bb8f242ea067804617f7886dfebfe
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-03-18 03:05:50 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+  #include <stdio.h>
+/**
+ *main - programs starts here
+ *Description : a program to print all alphabets except q and e
+ *Return : it returns O
+*/
+
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	do
+	{
+		if ((ch == 'e') || (ch == 'q'))
+		{
+			ch++;
+			continue;
+		}
+		putchar(ch);
+		ch++;
+	}
+	while (ch <= 'z');
+	putchar('\n');
+	return (0);
+}
