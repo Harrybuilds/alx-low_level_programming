357bc500bb6566de85a61f37e293f6ffb8d994f7
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-03-21 16:00:17 -0700

    int _abs(int)

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs - get absolute value of a number
+ *@n: paramete⅚r to be worked on
+ *Return: some kimd of return
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
