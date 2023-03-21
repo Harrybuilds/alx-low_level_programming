4bf0423758b6e8612fe068624a0104c41644f536
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-03-21 08:19:15 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - start of program
+ * Description: prints a to z using _printchar
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char let;
+
+	for (let = 'a'; let <= 'z'; let++)
+	{
+		_putchar(let);
+	}
+	_putchar(\n);
+	return (0);
+}
