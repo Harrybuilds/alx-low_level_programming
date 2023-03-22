d289620a3f9361223775e5fffbdfa6f0030d12cc
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-03-22 08:47:48 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *add - starts here
+ *@n: first number
+ *@m: second number
+ *Return: returns the result
+ */
+
+int add(int n, int m)
+{
+	int result;
+
+	result = m + n;
+
+	return (result8);
+}
