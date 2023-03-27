6d4a90b0ec93d06c3a45ee0f872ba12fceb1f7bf
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-03-27 09:19:14 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *puts - function to print character and a new line
+ *@str: variable parameter holding character to be printed
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+	a++;
+	}
+	_putchar('\n');
+
+
+}
