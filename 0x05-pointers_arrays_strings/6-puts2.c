743058aad9a66d3a505ccb4543b2d061b202d5a2
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-03-28 05:38:50 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *puts2 - function ro prints parts of a string
+ *@str: expected input to be manupulated
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}
