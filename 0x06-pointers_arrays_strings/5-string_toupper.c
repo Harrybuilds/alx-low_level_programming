2b95598d9bc3294589a0ec599e72d81af98e6082
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-03-30 01:01:11 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *string_toupper - converts lower case to uppercase
+ *@n: string to be passed
+ *Return: returns a upper case string
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+		}
+		i++;
+	}
+	return (n);
+}
