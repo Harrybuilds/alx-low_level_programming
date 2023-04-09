cee5b3b6427f836da2b6aa5acb4b32443eadcb8e
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-04-09 03:13:30 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - starting point of program execution
+ *@argc: command line argument counter
+ *@argv: command line strings stored in array
+ *Return: returns 0when program runs successful
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
