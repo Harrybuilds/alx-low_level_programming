44d58c2c73bc03bd3ccd51e4f41b351ecd888ba8
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-04-09 04:26:11 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
  *main - starting point of program execution
  *@argc: command line argument counter
@@ -7,9 +7,9 @@
  *Return: returns 0when program runs successful
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
