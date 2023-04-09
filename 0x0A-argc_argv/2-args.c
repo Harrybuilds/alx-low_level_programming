c104788c6a4910ee090a8ebb188a7a179fc352ac
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-04-09 07:11:35 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - program execution function
+ *@argc: command line argument counter
+ *@argv: command line string stored in array
+ *Return: return value after program execution
+ */
+int main(int argc __attribute__((unused)),char *argv[])
+{
+	int i = 1;
+
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			printf("%s\n", argv[i]);
+			i++;
+		}
+	}
+	return (0);
+}
