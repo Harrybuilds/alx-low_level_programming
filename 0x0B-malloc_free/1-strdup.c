0082e0f00b5f2f4c9c11327c7fe049985d8e95c1
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-04-12 06:50:08 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - function that copies a string passed to a pointer
+ * @str: is a pointer
+  * Return: returns a string copy
+ * On error, -1 is returned, and errno is set appropriately
+ */
+char *_strdup(char *str)
+{
+	char * copy;
+	unsigned int i = 0 , length = 0;
+
+	if (str == 0)
+		return (NULL);
+
+	while (str[length])
+		length++;
+
+	copy = malloc (sizeof(char) * (length +1));
+
+	while ((copy[i] = str[i]) != '\0')
+		i++;
+
+	return (copy);
+}
