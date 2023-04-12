902a71ec4df46f4e9eec1ac08bf913f695cd4f63
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-04-12 07:20:16 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,8 +8,8 @@
  */
 char *_strdup(char *str)
 {
-	char * copy;
-	unsigned int i = 0 , length = 0;
+	char *copy;
+	unsigned int i = 0, length = 0;
 
 	if (str == 0)
 		return (NULL);
@@ -17,14 +17,13 @@ char *_strdup(char *str)
 	while (str[length])
 		length++;
 
-	copy = malloc (sizeof(char) * (length +1));
-
-	if ((copy[i] = str[i]) == 0)
-		return (NULL);
+	copy = malloc(sizeof(char) * (length +1));
 
 	while ((copy[i] = str[i]) != '\0')
 		i++;
 
+	if ((copy[i] = str[i]) == 0)
+		return (NULL);
 
 	return (copy);
 
