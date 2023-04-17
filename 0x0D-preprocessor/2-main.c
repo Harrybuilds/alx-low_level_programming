598f116cc0f25b3a2aab44c19e514c6e65730bad
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-04-17 09:01:32 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - function to print name of file
+ *void: no expected argument
+ *Return: return an int data-type
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return(0);
+}
