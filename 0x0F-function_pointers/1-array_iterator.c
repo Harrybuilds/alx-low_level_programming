ac2bde607a2c6415c103328265a72ea387ad3d7a
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-04-19 06:18:04 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,9 +1,11 @@
  #include <stddef.h>
 /**
  *array_iterator - a function pointer that points to an array iterator function
-  *@arry: array to be passed to the function pointer 
+ *@array: array to be passed to the function pointer
  *@size: size of array being passed
- *@action: function that contains the manipulation/operation to be done on the array being passed
+ *@action: function that contains the manipulation/operation to be done on the
+ * array being passed
+ *
  *Return: does not expect any return value
  */
 
