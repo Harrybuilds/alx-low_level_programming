1c727a683b1d011b6e3377eb98fab0d2eea6626c
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-04-19 02:26:25 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ *print_name - a funtion that points to another function
+ *@name: The name to be manipulated on by the other funtion
+ *@f: The other function doing the manipulation
+ *Return: returns nothing after function execution
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
