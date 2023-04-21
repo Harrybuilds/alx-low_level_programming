5cd042e1f955b0d87f31ce3e6c0bc70521df47ce
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-04-21 03:16:16 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 
 		let = va_arg(strings, char *);
-		if (let == NULL)
+		if (let == 0)
 			printf("nil");
 		else
 			printf("%s", let);
