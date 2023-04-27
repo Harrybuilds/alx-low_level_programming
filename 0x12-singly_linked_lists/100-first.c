ce4e71f01832821fb57876d29ad9cda199fd9f06
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-04-27 11:59:02 -0700

    advance task 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ *start_up - function that executes before the main function
+ *@Constructor: the attribute that enables the function to executes befor main
+ *Return: doesn't expects any return value
+ */
+void start_up()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
