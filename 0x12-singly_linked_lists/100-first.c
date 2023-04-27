e29be401e22591acb48911c46fde5956040d7166
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-04-27 12:07:48 -0700

    task 100 edited to comply to betty standard

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,10 +2,11 @@
 
 /**
  *start_up - function that executes before the main function
- *@Constructor: the attribute that enables the function to executes befor main
+ *
  *Return: doesn't expects any return value
  */
-void start_up()
+void start_up(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,
+\nI bore my house upon my back!\n");
 }
