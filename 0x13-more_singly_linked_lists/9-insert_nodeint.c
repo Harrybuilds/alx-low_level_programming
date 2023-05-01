6af4590068c6ce206574932e466cb8a5e7a23992
Harrison Onuoha <harrisonchialuka@gmail.com>
2023-05-01 06:54:58 -0700

    task 9 of more singly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_holder = malloc(sizeof(listint_t));
 
-	if (!new_holder || !head)
+	if (new_holder || head)
 	{
 		new_holder->n = n;
 
