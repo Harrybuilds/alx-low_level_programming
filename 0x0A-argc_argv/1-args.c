323268ebc9516976c7044ab9379ddbd053b5e6c4
Harrison <harrisonchialuka@gmail.com>
2023-06-10 00:04:02 -0700

    task 1 of argc argv project

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program to print out number of argument passed to it
+ * @argc: arguments counter
+ * @argv:  arguments vector
+ * Return: returns 0 on success
+ */
+
+int main(__attribute__((__unused__))int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
