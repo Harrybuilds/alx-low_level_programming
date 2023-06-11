d9bb5fcc8e845efd1ac6373b4983558975adc358
Harrison <harrisonchialuka@gmail.com>
2023-06-11 08:39:52 -0700

    task 4 of argc and argv project

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include "main.h"
 
 /**
  * main - program to add numbers
@@ -23,7 +22,7 @@ int main(int argc, char *argv[])
 	{
 		int x = 0;
 
-		while (x < strlen(argv[i]))
+		while (x < _strlen(argv[i]))
 		{
 			if (argv[i][x] >= 48 && argv[i][x] <= 57)
 				x++;
@@ -40,12 +39,64 @@ int main(int argc, char *argv[])
 
 	while (i < argc)
 	{
-		sum += atoi(argv[i]);
+		if (_atoi(argv[i])  >= 0)
+			sum += _atoi(argv[i]);
 		i++;
-	}
+	} 
 
 	printf("%d\n", sum);
 
 	return (0);
 }
 
+
+
+
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
