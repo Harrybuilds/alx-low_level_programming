f1047f5f192e1e92a1a3936c52ba8141fd53ee77
Harrison <harrisonchialuka@gmail.com>
2023-06-19 14:37:48 -0700

    task 1 of structure and typedef project

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ *init_dog - function to initialize a struct dog
+ *@d: pointer to struct dog
+ *@name: member of the struct dog representing name
+ *@age: member of the struct dog representing age
+ *@owner: member of the struct dog representing owner
+ *Return: no expected return expected
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
