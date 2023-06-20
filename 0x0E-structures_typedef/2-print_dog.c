0d6bbcdf599e0915c132ba8eec1e5915788f1242
Harrison <harrisonchialuka@gmail.com>
2023-06-20 07:39:46 -0700

    task 2 of struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ *print_dog - prints details of struct dog type variable address passed to it
+ *@d: variable address of struct dog type, which details is to be printed
+ *Return: no expected return value
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		d->name = "(nil)";
+
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+
+}
