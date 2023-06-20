b22a67eb1893df95a8bcc66a04c4ed2c819fd3c3
Harrison <harrisonchialuka@gmail.com>
2023-06-20 15:24:02 -0700

    task 5 of struct and typedef project

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ *free_dog - function to free a struct object
+ *@d: struct object to be freed
+ *Return: does not expect any return value
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
