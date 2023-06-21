186225500cd236c77dec1069be0cb94068539e26
Harrison <harrisonchialuka@gmail.com>
2023-06-21 14:14:17 +0000

    second task for function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ *array_iterator - function to interate through an array and perform a task
+ *@array: the array to be iterated through
+ *@size: size of the array passed
+ *@action: function pointer to the function to be invoked
+ *Return: no return value expected
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+
+}
