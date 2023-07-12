62787edaff095fea417f1dbd9fa14eb23e998237
Harrison <harrisonchialuka@gmail.com>
2023-07-12 11:31:50 +0000

    correction on task 1 of file i/o in c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-  #include "main.h"
+   #include "main.h"
 
 /**
  *create_file - function to create a file
@@ -10,7 +10,9 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	mode_t mode;
 	size_t strlength, n;
+	int r;
 	char *str;
 	FILE *file;
 
@@ -33,6 +35,12 @@ int create_file(const char *filename, char *text_content)
 		}
 		fclose(file);
 	}
+
+	/*setting file permission for file owner*/
+	mode = S_IRUSR | S_IWUSR;
+	r = chmod(filename, mode);
+	if (r != 0)
+		return (-1);
 
 	return (1);
 }
