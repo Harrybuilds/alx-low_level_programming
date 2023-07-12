bbc66925d96bf19d5e22b8ca3d0b15c2ffdce4fd
Harrison <harrisonchialuka@gmail.com>
2023-07-12 12:39:22 +0000

    still struggling with task 1 of file i/o in c project

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,25 +21,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	str = text_content;
-/**<<<<<<< HEAD
- *	if (str != NULL)
- *	{
- *		strlength = strlen(str);
- *		n = dprintf(fileno(file), "%s", str);
- *		if (n != strlength)
- *		{
- *			fclose(file);
- *			return (-1);
- *		}
- *		fclose(file);
- *	}
- *=======
- *	dprintf(fileno(file), "%s", str);
- *	fclose(file);
- *>>>>>>> parent of 12f0a02 (correction on task 1 of file i/o in c)
- */
-
-
 	if (str != NULL)
 	{
 		len = strlen(str);
@@ -50,11 +31,6 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 	}
-			
-/**	dprintf(fileno(file), "%s", str);
-	fclose(file);
-*/
-	
 	/*setting file permission for file owner*/
 	mode = S_IRUSR | S_IWUSR;
 	r = chmod(filename, mode);
