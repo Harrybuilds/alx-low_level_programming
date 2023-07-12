12f0a021a33d921c5c39996a98d412d265ac2294
Harrison <harrisonchialuka@gmail.com>
2023-07-12 10:28:47 +0000

    correction on task 1 of file i/o in c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  *create_file - function to create a file
  *@filename: name of file to be created
- *@text_content: string to be inserted into the file
+ *@text_content: null terminated string to be written into the file
  *
  *Return: returns 1 on success else -1
  */
@@ -13,13 +13,22 @@ int create_file(const char *filename, char *text_content)
 	char *str;
 	FILE *file;
 
-	file = fopen(filename, "w");
+	file = fopen(filename, "r");
 	if (filename == NULL || file == NULL)
 		return (-1);
 
 	str = text_content;
-	dprintf(fileno(file), "%s", str);
-	fclose(file);
+	if (str == NULL)
+	{
+		file = fopen(filename, "w");
+		fclose(file);
+	}
+	else
+	{
+		file = fopen(filename, "w");
+		dprintf(fileno(file), "%s", str);
+		fclose(file);
+	}
 
 
 	return (1);
