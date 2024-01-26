3d377028d2e727f858e575c60ee48027e4b49333
Harrison <harrisonchialuka@gmail.com>
2024-01-26 18:44:02 +0100

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * sum_dlistint : function to get sum of nodes in the list
+ * @head: start point of the list
+ * Return: sum of all nodes in the list
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
