02ee2e4e628fd2baa13af93ec31015171dfceb3d
Harrison <harrisonchialuka@gmail.com>
2024-01-26 14:41:10 +0100

    modified header file

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -13,28 +13,16 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 
 /* Macro to create a new node */
-#define CREATE_NODE(data)(
-{
-	dlistint_t* newNode = malloc(sizeof(dlistint_t));
-	if (newNode != NULL)
-	{
-		newNode->data = (data);
-		newNode->next = NULL;
-		newNode->prev = NULL;
-	}                
-	newNode;
-})
 
 
 size_t print_dlistint(const dlistint_t *h);
