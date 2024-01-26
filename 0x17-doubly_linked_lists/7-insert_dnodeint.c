19977c4dadae9b2a2a16d354381860e148d61933
Harrison <harrisonchialuka@gmail.com>
2024-01-26 19:05:09 +0100

    correction on task 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,86 +1,76 @@
 #include "lists.h"
 
-/**
- * CreateNode - function to create a new doubly linked list  node
- * @data: data to be inserted into the node
- * Return: address of new node if succesful else NULL
- */
-
-dlistint_t *CreateNode(int data)
-{
-	dlistint_t *newNode = malloc(sizeof(dlistint_t));
-
-	if (newNode != NULL)
-	{
-		newNode->n = (data);
-		newNode->next = NULL;
-		newNode->prev = NULL;
-	}
-	return (newNode);
+/* Function to create a new node*/
+dlistint_t *CreateNode(int data) {
+    dlistint_t *newNode = malloc(sizeof(dlistint_t));
+    if (newNode != NULL) {
+        newNode->n = data;
+        newNode->prev = NULL;
+        newNode->next = NULL;
+    }
+    return newNode;
 }
 
-
 /**
- * insert_dnodeint_at_index - add node at a specified index in list
- * @h: head node of list
- * @ind: position in list node is tobe inserted into
- * @n: data to be insertedinto the node
- * Return: address of node inserted in the list
+ * insert_dnodeint_at_index - Insert a new node at a given position in a doubly linked list
+ * @h: Pointer to the head node of the list
+ * @idx: Index where the new node should be added
+ * @n: Data to be inserted into the new node
+ * Return: Address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int ind = 0;
-	dlistint_t *beforeTarget, *target;
+	dlistint_t *newNode = CreateNode(n);
+	dlistint_t *current = *h;
 
-	if (*h != NULL)
+
+	if (h == NULL)
 	{
-		dlistint_t *newNode = CreateNode(n);
-
-		if (newNode != NULL)
-		{
-			while (*h != NULL)
-			{
-				/* get node before target index and target index*/
-				if (ind == idx-1)
-				{
-					beforeTarget = *h;
-				}
-				else if (ind == idx)
-				{
-					target = *h;
-				}
-				*h = (*h)->next;
-				ind++;
-			}
-
-			if (idx == 0)
-			{
-				/*insert at the begining ofthe list*/
-				newNode->next = *h;
-				if (*h != NULL)
-				{
-					(*h)->next = newNode;
-				}
-				*h = newNode;
-			}
-			else if(ind == idx)
-			{
-				/* insert at the end of the list */
-				beforeTarget->next = newNode;
-				newNode->prev = beforeTarget;
-			}
-			else
-			{
-				/*insert at the middle of the list */
-				beforeTarget->next = newNode;
-				newNode->prev = beforeTarget;
-				newNode->next = target;
-				target->prev = newNode;
-			}
-
-		return (newNode);
-		}
+		return (NULL);
 	}
-	return (NULL);
+
+	if (newNode == NULL)
+	{
+		return (NULL); /* Memory allocation failed */
+	}
+
+	if (idx == 0)
+	{
+		/* Inserting at the beginning of the list*/
+		newNode->next = *h;
+		if (*h != NULL)
+		{
+			(*h)->prev = newNode;
+		}
+		*h = newNode;
+	}
+	else
+	{
+		/* Inserting at a position other than the beginning*/
+
+		while (current != NULL && ind < idx - 1)
+		{
+			current = current->next;
+			ind++;
+		}
+
+		if (current == NULL || ind < idx - 1)
+		{
+			/* Index out of bounds */
+			free(newNode);
+			return (NULL);
+		}
+
+		newNode->next = current->next;
+		newNode->prev = current;
+		if (current->next != NULL)
+		{
+			current->next->prev = newNode;
+		}
+		current->next = newNode;
+	}
+
+	return (newNode);
 }
+
