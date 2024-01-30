cb4fafa95904868a4dbce8c5448ceb96f480527a
Harrison <harrisonchialuka@gmail.com>
2024-01-30 10:59:40 +0100

    empty functions

---
+++ 0x18-dynamic_libraries/file1.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ *_isdigit -function to check if a character is a digit
+ * @c: character to be checked
+ *Return: returns an integer representing weather it is or not
+*/
+int _isdigit(int c)
+{
+	return (0);
+}
+
+/**
+ *_strlen -function to check length of a string
+ * @s: string to be checked
+ *Return: returns an integer representing the length of the string
+*/
+int _strlen(char *s)
+{
+	return (0);
+}
+
+/**
+ *_puts -function to print a string
+ * @s: string to be printed
+ *Return: nothing is returned
+*/
+void _puts(char *s)
+{
+}
+
+/**
+ *_strcpy -function to copy the contents of a string into another space
+ * @dest: location where string is to be copied to
+ * @src: string being copied
+ *Return: returns pointer to string copied
+*/
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr;
+
+	return (ptr);
+}
+
+/**
+ *_atoi - function to convert string to integer
+ * @s: string to be converted
+ * Return: returns the integer converted to
+*/
+int _atoi(char *s)
+{
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/file4.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ *_memcpy - function to copy content in a memory space
+ * @dest: destination for content to be copied to
+ * @src: source where content are to be copied from
+ * @n: number of character to be copied
+ * Return: pointer to destination content
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *ptr;
+
+	return (ptr);
+}
+
+
+/**
+ *_strchr - function to
+ * @s: string
+ * @c: character
+ * Return: a pointer to
+*/
+char *_strchr(char *s, char c)
+{
+	char *ptr;
+
+	return (ptr);
+}
+
+/**
+ *_strspn - function to
+ * @s: string to be
+ * @accept: string
+ * Return: unsigned int
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	return (0);
+}
+
+
+/**
+ *_strpbrk - function to
+ * @s: string to be
+ * @accept: string
+ * Return: pointer to
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	char *ptr;
+
+	return (ptr);
+}
+
+
+/**
+ *_strstr - function to
+ * @haystack: string to be
+ * @needle: string
+ * Return: pointer to
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	char *ptr;
+
+	return (ptr);
+}
